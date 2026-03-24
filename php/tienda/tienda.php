89331657428602518bc990cad36357f910054921
Miikeel16 <mikel.garcia06@somo.eus>
2026-03-24 12:49:23 +0100

    notis en tienda

--- php/tienda/tienda.php
+++ php/tienda/tienda.php
@@ -82,12 +82,30 @@ if (isset($_SESSION['id_usuario'])) {
         </a>
 
     <?php else: ?>
+        <div class="user-actions">
+            <div class="notif-wrapper">
+                <div id="bell-icon">
+                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M12 22C13.1 22 14 21.1 14 20H10C10 21.1 10.9 22 12 22ZM18 16V11C18 7.93 16.37 5.36 13.5 4.68V4C13.5 3.17 12.83 2.5 12 2.5C11.17 2.5 10.5 3.17 10.5 4V4.68C7.64 5.36 6 7.92 6 11V16L4 18V19H20V18L18 16Z" fill="currentColor"/>
+                    </svg>
+                    <span id="notif-badge">0</span>
+                </div>
 
-        <a class="tag" href="../../php/user/perfiles/perfilSesion.php">
-            <?php echo htmlspecialchars($_SESSION['tag']); ?>
-        </a>
-
+                <div id="notif-dropdown">
+                    <div class="notif-header">
+                        <span>Notificaciones</span>
+                        <button onclick="marcarLeidas()">Limpiar</button>
+                    </div>
+                    <ul id="notif-list"></ul>
+                </div>
+            </div>
+            <a class="tag" href="../../php/user/perfiles/perfilSesion.php">
+                <?php echo htmlspecialchars($_SESSION['tag']); ?>
+            </a>
+        </div>
     <?php endif; ?>
+
+        <script src="../../js/notificaciones.js"></script>
 
 </header>
 
