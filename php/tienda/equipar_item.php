8c8c6e67ffde7dc2fb58b4faf0f8f7949284b20c
Ion <ion.dieguez06@somo.eus>
2026-05-08 12:02:47 +0200

    arreglados equipar items y responsive perfilotros

--- php/tienda/equipar_item.php
+++ php/tienda/equipar_item.php
@@ -2,22 +2,25 @@
 session_start();
 require '../../db/conexiones.php';
 
-$id_usuario = $_SESSION['id_usuario'];
+$id_usuario = (int)$_SESSION['id_usuario'];
 $id_item = (int) $_POST['id_item'];
 
 mysqli_begin_transaction($conexion);
 
 try {
 
-    // Obtener tipo del item
+    // 1. EL ARREGLO: Sacamos el tipo Y LA IMAGEN del item
     $res = mysqli_query($conexion, "
-        SELECT tipo FROM Tienda_Items WHERE id_item = $id_item
+        SELECT tipo, imagen FROM Tienda_Items WHERE id_item = $id_item
     ");
-
+    
     $item = mysqli_fetch_assoc($res);
-    $tipo = $item['tipo'];
+    if (!$item) throw new Exception("Item no encontrado");
 
-    // Desequipar todos los del mismo tipo
+    $tipo = $item['tipo'];
+    $imagen = $item['imagen']; // Ej: 'marcos/1.png'
+
+    // Desequipar todos los del mismo tipo en el inventario
     mysqli_query($conexion, "
         UPDATE Usuario_Items ui
         JOIN Tienda_Items ti ON ti.id_item = ui.id_item
@@ -34,19 +37,18 @@ try {
         AND id_item = $id_item
     ");
 
-    // Guardar en Usuario (CONTROLADO)
+    // 2. Guardar en Usuario: Actualizamos tanto el ID activo como la ruta de la imagen
     if ($tipo === 'avatar') {
-        mysqli_query($conexion, "UPDATE Usuario SET avatar_activo = $id_item WHERE id_usuario = $id_usuario");
+        mysqli_query($conexion, "UPDATE Usuario SET avatar_activo = $id_item, avatar = '$imagen' WHERE id_usuario = $id_usuario");
     }
 
     if ($tipo === 'marco') {
-        mysqli_query($conexion, "UPDATE Usuario SET marco_activo = $id_item WHERE id_usuario = $id_usuario");
+        mysqli_query($conexion, "UPDATE Usuario SET marco_activo = $id_item, marco_avatar = '$imagen' WHERE id_usuario = $id_usuario");
     }
 
     if ($tipo === 'fondo') {
         mysqli_query($conexion, "UPDATE Usuario SET fondo_activo = $id_item WHERE id_usuario = $id_usuario");
     }
-
 
     mysqli_commit($conexion);
 
@@ -56,3 +58,4 @@ try {
 
 header("Location: inventario.php");
 exit;
+?>
